158fb1de0fdf51d4d9efc2f5a445de799553ac70
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-03-14 15:19:44 +0000

    Added pics to the projects.json

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,6 +16,14 @@ function Contact() {
         conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
         rhoncus. Etiam vel condimentum magna, quis tempor nulla.
       </p>
+      {/* <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div> */}
     </div>
   );
 }
